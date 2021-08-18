ce9f81078168000079aa97aa49dc7e9ef5000092
takusuru <taku.a.k.a.mizutani@gmail.com>
2021-08-18 08:59:32 +0000

    [Create] customersテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :customers
   devise_for :admin
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     root to: 'homes#top'
   end
+
 end

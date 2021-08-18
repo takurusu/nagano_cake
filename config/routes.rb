72e6393619ad5d7797457051da538d64ed69bc71
takusuru <taku.a.k.a.mizutani@gmail.com>
2021-08-18 08:32:24 +0000

    [Create, Add] admin/homes_controllerの作成, ルーティングにadminのルートパス設定, top.html.erbの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    root to: 'homes#top'
+  end
 end

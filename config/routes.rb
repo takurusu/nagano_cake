abd74e35cb9b3139e7ff5811a607ed0c2a41eabc
takusuru <taku.a.k.a.mizutani@gmail.com>
2021-08-19 18:24:57 +0000

    Add 'config.scoped_views = true' to devise.rb and change routes.rb for views:admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :customers
-  devise_for :admin 
+  devise_for :admins, path: :admin, views: {
+    :registrations => 'admins/registrations',
+    :sessions => 'admins/sessions'
+  }
 
   namespace :admin do
-   root to: 'homes#top' 
+   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 

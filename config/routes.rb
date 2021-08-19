3af4fa13cb6ac45cfddf7f8417bfa0d8d2263561
takusuru <taku.a.k.a.mizutani@gmail.com>
2021-08-19 17:58:21 +0000

    [Add] headerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :customers
-  devise_for :admin
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admin 
+
   namespace :admin do
-    root to: 'homes#top'
+   root to: 'homes#top' 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 
 end
